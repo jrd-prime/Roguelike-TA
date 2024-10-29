038dc02d50f54652a8be35b321570cb4a92464bf
jrd-prime <three.jrd@gmail.com>
2024-10-29 17:02:10 +0500

    -comm

--- Assets/Game/Scripts/Framework/Managers/Experience/ExperienceManager.cs
+++ Assets/Game/Scripts/Framework/Managers/Experience/ExperienceManager.cs
@@ -17,20 +17,14 @@ namespace Game.Scripts.Framework.Managers.Experience
             Debug.LogWarning("Experience manager awake");
             if (baseExpToLevelUp <= 0) baseExpToLevelUp = 100f;
             ExpToNextLevel.Value = baseExpToLevelUp;
-                
         }
 
         public void AddExperience(float experience)
         {
-            Debug.LogWarning($"Add experience: {experience}");
             if (experience <= 0) return;
-
-            Debug.LogWarning(
-                $"Current exp: {CurrentExp.CurrentValue} exp to next level: {ExpToNextLevel.CurrentValue}");
 
             if (CurrentExp.CurrentValue + experience >= ExpToNextLevel.CurrentValue)
             {
-                Debug.LogWarning("Level up????");
                 OnLevelUp(experience);
                 return;
             }
@@ -40,10 +34,7 @@ namespace Game.Scripts.Framework.Managers.Experience
 
         private void OnLevelUp(float experience)
         {
-            Debug.LogWarning("Level up");
-
             CurrentExp.Value = CurrentExp.CurrentValue + experience - ExpToNextLevel.CurrentValue;
-
             Level.Value += 1;
             ExpToNextLevel.Value *= multiplierPerLevel;
         }
